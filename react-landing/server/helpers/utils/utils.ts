7b6f66b3ae36615da227f6c86fcfbabc9b09648b
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2024-10-16 18:17:27 +0200

    Update utils.ts

--- react-landing/server/helpers/utils/utils.ts
+++ react-landing/server/helpers/utils/utils.ts
@@ -8,36 +8,46 @@ import { type FileNames } from "../types/data-types";
 import { FILES_MAP, type UsersData } from "../types/data-types";
 
 export const readWriteFile = async <T>(fileName: FileNames) => {
-  const filePath = path.join('/tmp', "data", fileName);
+    const filePath = path.join('/tmp', "data", fileName);
 
-  const readFile = async (): Promise<T> =>
-    JSON.parse(await fs.readFile(filePath, "utf-8"));
+    await fs.mkdir(path.dirname(filePath), { recursive: true });
 
-  const writeFile = async (data: T): Promise<void> =>
-    fs.writeFile(filePath, JSON.stringify(data), "utf-8");
+    const readFile = async (): Promise<T> => {
+        try {
+            const data = await fs.readFile(filePath, "utf-8");
 
-  return [readFile, writeFile] as const;
+            return JSON.parse(data);
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    const writeFile = async (data: T): Promise<void> => {
+        await fs.writeFile(filePath, JSON.stringify(data), "utf-8");
+    };
+
+    return [readFile, writeFile] as const;
 };
 
 export const saveUser = async () => {
-  const id = cookies().get("userId")?.value;
+    const id = cookies().get("userId")?.value;
 
-  if (!id) throw new Error("No session created");
+    if (!id) throw new Error("No session created");
 
-  const fileName = FILES_MAP.users.path;
+    const fileName = FILES_MAP.users.path;
 
-  const [readUsers, writeUsers] = await readWriteFile<UsersData>(fileName);
+    const [readUsers, writeUsers] = await readWriteFile<UsersData>(fileName);
 
-  try {
-    const users = await readUsers();
-    const user = users.find((user) => user.id === +id);
+    try {
+        const users = await readUsers();
+        const user = users.find((user) => user.id === +id);
 
-    if (user) return;
+        if (user) return;
 
-    const updatedUsers = [...users, { id: +id, cart: [], favorites: [] }];
+        const updatedUsers = [...users, { id: +id, cart: [], favorites: [] }];
 
-    await writeUsers(updatedUsers);
-  } catch (error) {
-    throw error;
-  }
+        await writeUsers(updatedUsers);
+    } catch (error) {
+        throw error;
+    }
 };
